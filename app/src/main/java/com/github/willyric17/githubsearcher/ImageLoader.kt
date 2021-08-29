c2c51e3fa0b6a4d429e3eea72210a837dafb5545
Willy Ricardo Wijaya <willyric17@gmail.com>
2021-08-29 14:38:07 +0700

    create splash screen

---
+++ app/src/main/java/com/github/willyric17/githubsearcher/ImageLoader.kt
@@ -0,0 +1,11 @@
+package com.github.willyric17.githubsearcher
+
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.bumptech.glide.Glide
+
+fun ImageView.load(@DrawableRes resId: Int) {
+    Glide.with(this)
+        .load(resId)
+        .into(this)
+}
--- app/src/main/java/com/github/willyric17/githubsearcher/splash/SplashActivity.kt
+++ app/src/main/java/com/github/willyric17/githubsearcher/splash/SplashActivity.kt
@@ -1,7 +1,45 @@
 package com.github.willyric17.githubsearcher.splash
 
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.github.willyric17.githubsearcher.R
+import com.github.willyric17.githubsearcher.databinding.ActivitySplashBinding
+import com.github.willyric17.githubsearcher.load
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+            .also { setContentView(it.root) }
+
+    }
+
+    lateinit var splashTimer: Disposable
+
+    override fun onStart() {
+        super.onStart()
+        splashTimer = Completable.timer(500, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnComplete {
+                binding.image.apply {
+                    load(R.mipmap.octocat)
+                    startAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_in))
+                }
+            }
+            .delay(2, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                Toast.makeText(this, "DONE", Toast.LENGTH_SHORT).show()
+            }
+    }
 }
